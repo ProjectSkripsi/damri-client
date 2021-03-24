7dbd64f341235c835f067939db98644cd61f5e5f
arul21 <arul.21@hotmail.com>
2021-03-24 10:20:51 +0700

    favicon

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -106,16 +106,16 @@ export default {
 </script>
 
 <style scoped>
-.about {
-  background: url('https://static.vecteezy.com/system/resources/thumbnails/000/626/167/original/Geometric_abstract_grey_background_for_bussines_templates_website_wallpapers.jpg') no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  background-size: cover;
-  -o-background-size: cover;
- 
-}
-.rows {
-   padding-bottom: 80px;
-}
+  .about {
+    background: url('https://static.vecteezy.com/system/resources/thumbnails/000/626/167/original/Geometric_abstract_grey_background_for_bussines_templates_website_wallpapers.jpg') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    background-size: cover;
+    -o-background-size: cover;
+  
+  }
+  .rows {
+    padding-bottom: 80px;
+  }
 </style>
 
